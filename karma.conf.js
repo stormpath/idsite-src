c6707d4d699eaff657d2c75c855587ec3e3ce1f0
Robert <robert@stormpath.com>
2014-05-05 14:03:29 -0700

    include stormpath vender in tests

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
       'app/bower_components/angular-route/angular-route.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
+      'app/vendor/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
