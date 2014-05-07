1df94c7d5d238eaa345ccc85006e654348bc60f9
Robert <robert@stormpath.com>
2014-05-07 10:52:04 -0700

    validation for forgot form

--- app/scripts/controllers/forgot.js
+++ app/scripts/controllers/forgot.js
@@ -3,9 +3,16 @@
 angular.module('stormpathIdpApp')
   .controller('ForgotCtrl', function ($scope,Stormpath) {
     $scope.sent = false;
+    $scope.fields = {};
     $scope.submit = function(){
       $scope.notFound = false;
-      Stormpath.sendPasswordResetEmail($scope.email,function(err,resp){
+      var inError = Object.keys($scope.fields).filter(function(f){
+        return $scope.fields[f].validate();
+      });
+      if(inError.length>0){
+        return;
+      }
+      Stormpath.sendPasswordResetEmail($scope.fields.email.value,function(err){
         if(err){
           if(err.status===404){
             $scope.notFound = true;
@@ -13,7 +20,6 @@ angular.module('stormpathIdpApp')
             $scope.unknownError = err;
           }
         }else{
-          // debugger
           $scope.sent = true;
         }
       });
