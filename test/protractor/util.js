8a507a70d1ef79cefa0391ad041285b1a77613fe
Robert <robert@stormpath.com>
2014-05-12 17:51:58 -0700

    util method for last commit

--- test/protractor/util.js
+++ test/protractor/util.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function uuid(){
+  return Math.floor(Math.random()*10000)+'';
+}
+
 module.exports = {
   getCurrentUrl: function(cb) {
     // workaround for this issue:
@@ -7,5 +11,9 @@ module.exports = {
     // https://github.com/angular/protractor/issues/132
 
     browser.executeScript('return window.document.location.href').then(cb);
+  },
+  fakeAuthParams: function(){
+    return '&access_token=' + uuid() +
+      '&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234';
   }
 };
