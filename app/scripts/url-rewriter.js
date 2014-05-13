088fe909909d225d8cf1e44a1674d54b89edb147
Robert <robert@stormpath.com>
2014-05-13 11:08:59 -0700

    more url rewrite cases

--- app/scripts/url-rewriter.js
+++ app/scripts/url-rewriter.js
@@ -8,9 +8,10 @@
     // the $location.search() api
     var match = url.match(/([^#&?]+=[^#&?]+)/g,'?$1');
     if(match){
-      var a = url.replace(match.join('&'),'');
-      a = a.replace(/[&?]$/,'');
-      return a + '?' + match.join('&');
+      var b = match.join('&').replace(/^\//,'');
+      var a = url.replace(b,'');
+      a = a.replace(/[&?\/]$/,'');
+      return a + '?' + b;
     }else{
       return url;
     }
--- test/spec/services/urlfixer.js
+++ test/spec/services/urlfixer.js
@@ -22,16 +22,28 @@ describe('URL rewriter', function () {
       expect: 'http://local.stormpath.com:9000/#register&',
     },
     {
-      url:    'http://local.stormpath.com:9000/#code=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234',
-      expect: 'http://local.stormpath.com:9000/#?code=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234',
+      url:    'http://local.stormpath.com:9000/#/access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234',
+      expect: 'http://local.stormpath.com:9000/#?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234',
     },
     {
-      url:    'http://local.stormpath.com:9000/#reset&code=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=aaa',
-      expect: 'http://local.stormpath.com:9000/#reset?code=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=aaa'
+      url:    'http://local.stormpath.com:9000/#access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234',
+      expect: 'http://local.stormpath.com:9000/#?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234',
     },
     {
-      url:    'http://local.stormpath.com:9000/#reset?code=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=vvv',
-      expect: 'http://local.stormpath.com:9000/#reset?code=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=vvv'
+      url:    'http://local.stormpath.com:9000/#reset&access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=aaa',
+      expect: 'http://local.stormpath.com:9000/#reset?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=aaa'
+    },
+    {
+      url:    'http://local.stormpath.com:9000/#/reset&access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=aaa',
+      expect: 'http://local.stormpath.com:9000/#/reset?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=aaa'
+    },
+    {
+      url:    'http://local.stormpath.com:9000/#reset?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=vvv',
+      expect: 'http://local.stormpath.com:9000/#reset?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=vvv'
+    },
+    {
+      url:    'http://local.stormpath.com:9000/#/reset?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=vvv',
+      expect: 'http://local.stormpath.com:9000/#/reset?access_token=1234&application_href=https%3A%2F%2Fapi.stormpath.com%2Fv1%2Fapplications%2F1234&sptoken=vvv'
     }
   ];
 
