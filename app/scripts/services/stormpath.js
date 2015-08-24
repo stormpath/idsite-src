d6c17c537d476c27ae110812e74c7bb40d560aa1
Robert <robert@stormpath.com>
2015-08-24 16:33:58 -0700

    use base url

--- app/scripts/services/stormpath.js
+++ app/scripts/services/stormpath.js
@@ -29,7 +29,7 @@ angular.module('stormpathIdpApp')
     }
 
     function redirect(url){
-      $window.location = 'https://api.stormpath.com/sso/?jwtResponse='+url.split('jwtResponse=')[1];
+      $window.location = client.baseUrl + '?jwtResponse='+url.split('jwtResponse=')[1];
     }
 
     function initialize(){
