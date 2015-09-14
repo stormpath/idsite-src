389d71bce0aedc25301140d43f4b6d9fa9500c07
Robert <robert@stormpath.com>
2015-09-14 12:58:22 -0700

    preparing for merge

--- Gruntfile.js
+++ Gruntfile.js
@@ -460,7 +460,7 @@ module.exports = function (grunt) {
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
-      // 'karma:liveunit', // liveunit tests disabled until I can fix the test cases
+      // 'karma:liveunit',  // disabled until karama test runner is fixed
       'watch'
     ]);
   });
