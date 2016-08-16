2581ba48600ee395756478b619233d6f57f833fb
Robert <robert@stormpath.com>
2016-08-16 11:23:22 -0700

    No-session-redirect example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,5 +35,22 @@
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .run(function(Stormpath, $window){
+    /**
+     * This error handler will redirect the user to the specified
+     * URL, if the arrive on ID Site without a session, e.g. they
+     * bookmarked the page, and are not coming to it via the
+     * service provider.  In this example, I redirect the user back
+     * to the /login endpoint on my service provider, which will
+     * issue a new redirect to ID Site, creating a new session.
+     */
+    Stormpath.init.catch(function(err){
+      var redirectUrl = 'http://localhost:3000/login';
+      if(err.message === 'Login session not initialized.') {
+        Stormpath.errors.splice(0,Stormpath.errors.length);
+        $window.location.replace(redirectUrl);
+      }
+    });
   });
 })(window);
